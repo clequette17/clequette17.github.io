f848eb5101ede6c3de6db1164e689fa8b6c8e9de
clequette17 <corentin.lequette@orange.fr>
2019-06-06 18:27:06 +0200

    test3

--- js/hamburger.js
+++ js/hamburger.js
@@ -1,12 +1,21 @@
-(function() {
-    document.querySelector('.hamburger').addEventListener('click', (e) => {
-        let links = document.querySelector('.links');
-        let linksDisplay = window.getComputedStyle(links);
-        if(linksDisplay['display'] == "none"){
-            links.style.display = "block";
-        }
-        else{
-            links.style.display = "none";
-        }
+(function ($) {
+    $('.hamburger').on('click', () => {
+        if ($('.links').css('display') == 'block') $('.links').css('display', 'none');
+        else $('.links').css('display', 'block');
     });
- })();
+    $(window).resize(function () {
+        if ($(window).outerWidth() > 930) $('.links').css('display', 'block');
+        else $('.links').css('display', 'none');
+    });
+    $('.links .link, .footer a').on('click', function (e) {
+        let top = $($(this).attr('href')).offset().top - $('.nav').outerHeight();
+        $('html').stop().animate({
+            scrollTop: top
+        }, 1000);
+    });
+    $(window).scroll(function () {
+        $('.section-head').each(function(){
+            console.log($(this).offset());
+        });
+    });
+})(jQuery);
