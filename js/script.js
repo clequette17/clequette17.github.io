8c2af04583748163085c0813c3a7dfb3334bddca
clequette17 <corentin.lequette@orange.fr>
2019-06-09 12:42:49 +0200

    test6

--- js/script.js
+++ js/script.js
@@ -4,11 +4,13 @@
             $('.links').css('display', 'none');
         else $('.links').css('display', 'block');
     });
+
     $(window).resize(function () {
         if ($(window).outerWidth() > 930)
             $('.links').css('display', 'block');
         else $('.links').css('display', 'none');
     });
+
     $('.links .link, .footer a').on('click', function (e) {
         if ($(window).outerWidth() <= 930) $('.links').css('display', 'none');
         let top = $($(this).attr('href')).offset().top - $('.nav').outerHeight();
@@ -16,6 +18,7 @@
             scrollTop: top
         }, 1000);
     });;
+
     $(window).scroll(function () {
         $('.presentation, .section-head, .section-text, .maps, .coords, .portfolio-item, .contact-form').each(function () {
             let elTop = $(this).offset().top;
@@ -23,12 +26,10 @@
             let scrollTop = $(window).scrollTop();
             let scrollBottom = scrollTop + $(window).height();
             if ((elTop > scrollTop && elTop < scrollBottom) || (elBottom > scrollTop && elBottom < scrollBottom)) {
-                if ($(this).hasClass('section-head')) $(this).addClass('anim-bounce-in-right');
-                if ($(this).hasClass('section-text') || $(this).hasClass('coords')) $(this).addClass('anim-bounce-in-left');
-                if ($(this).hasClass('presentation')) $(this).addClass('anim-opacity');
-                if ($(this).hasClass('portfolio-item')) $(this).addClass('anim-opacity');
-                if ($(this).hasClass('maps')) $(this).addClass('anim-scale');
-                if($(this).hasClass('contact-form')) $(this).addClass('anim-bounce-in-right');
+                if ($(this).hasClass('section-head') || $(this).hasClass('contact-form')) $(this).addClass('anim-fade-right');
+                if ($(this).hasClass('section-text') || $(this).hasClass('coords')) $(this).addClass('anim-fade-left');
+                if ($(this).hasClass('presentation')|| $(this).hasClass('portfolio-item')) $(this).addClass('anim-opacity');
+                if ($(this).hasClass('maps')) $(this).addClass('anim-fadeIn');
             }
         });
     });
