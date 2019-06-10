836df847233528857aef37afc2eef4b9decd25f9
clequette17 <corentin.lequette@orange.fr>
2019-06-10 02:37:32 +0200

    test9

--- js/script.js
+++ js/script.js
@@ -1,25 +1,17 @@
 (function ($) {
-    $('.hamburger').on('click', () => {
-        if ($('.links').css('display') == 'block')
-            $('.links').css('display', 'none');
-        else $('.links').css('display', 'block');
+    $('.hamburger').on('click', function () {
+        $('.nav').toggleClass('nav-toggle');
     });
 
-    $(window).resize(function () {
-        if ($(window).outerWidth() > 930)
-            $('.links').css('display', 'block');
-        else $('.links').css('display', 'none');
-    });
-
-    $('.links .link, .footer a').on('click', function (e) {
-        if ($(window).outerWidth() <= 930) $('.links').css('display', 'none');
+    $('.nav .link, .footer a').on('click', function (e) {
+        if ($('.nav').hasClass('nav-toggle')) $('.nav').removeClass('nav-toggle');
         let top = $($(this).attr('href')).offset().top - $('.nav').outerHeight();
         $('html').stop().animate({
             scrollTop: top
         }, 1000);
     });;
 
-    $('.presentation, .section-head, .section-text, .maps, .coords, .portfolio-item, .contact-form').each(function() {
+    $('.presentation, .section-head, .section-text, .maps, .coords, .portfolio-item, .contact-form').each(function () {
         $(this).css('visibility', 'hidden');
     });
 
@@ -32,7 +24,7 @@
             if ((elTop > scrollTop && elTop < scrollBottom) || (elBottom > scrollTop && elBottom < scrollBottom)) {
                 if ($(this).hasClass('section-head') || $(this).hasClass('contact-form')) $(this).addClass('anim-fade-right');
                 if ($(this).hasClass('section-text') || $(this).hasClass('coords')) $(this).addClass('anim-fade-left');
-                if ($(this).hasClass('presentation')|| $(this).hasClass('portfolio-item')) $(this).addClass('anim-opacity');
+                if ($(this).hasClass('presentation') || $(this).hasClass('portfolio-item')) $(this).addClass('anim-opacity');
                 if ($(this).hasClass('maps')) $(this).addClass('anim-fadeIn');
                 $(this).css('visibility', 'visible');
             }
